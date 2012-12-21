282267475934c574dcde965ea7ddf35876d1c2f7
jfax <fuchs@d-mind.de>
2012-12-21 14:28:35 +0100

    Fehlende Abfrage class_exists hinzugefuegt

--- classes/class.336news.inc.php
+++ classes/class.336news.inc.php
@@ -261,6 +261,7 @@ class rex_336_news {
             $pager['jumplist'] = self::drawJumplist ( rex_getUrl('','', array("page" => 'SEITENZAHL'),'&amp;'), "&lt;", " ", "&gt;", $conf['page'], $pnum  );
 		}
         
+        // http://www.redaxo.org/de/forum/addons-f30/news-addon-d-mind-t18730.html
         if (!class_exists('Smarty')) include('redaxo/include/addons/news/libs/Smarty.class.php');
         $t = new Smarty;
         $t->debugging = false;
