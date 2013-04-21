eabd0ad42306ff83a5627bf434db4c697a0a60fe
jfax <fuchs@d-mind.de>
2013-04-21 13:30:10 +0200

    Datenfeld stickyUntil ergänzt

--- files/js/backend.js
+++ files/js/backend.js
@@ -9,7 +9,8 @@ jQuery(document).ready(function() {
 			6: { sorter: false },
             7: { sorter: false },
             8: { sorter: false },
-            9: { sorter: false }
+            9: { sorter: false },
+		   10: { sorter: false }
 		}
 	}); 
     jQuery('.warningDisplayInfo').parent('p').find('textarea').addClass('warningDisplayInfoP');
